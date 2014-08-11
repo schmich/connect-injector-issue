4dcac6fbcbea223961e9ae5db93080976d75facf
Chris Schmich <schmch@gmail.com>
2014-08-11 11:57:50 -0500

    Adding a self-running test case for Node repro.

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var connect = require('connect');
 var http = require('http');
 var injector = require('connect-injector');
+var request = require('request');
+var assert = require('assert');
 
 var rewrite = injector(function(req, res) {
     return true;
@@ -13,7 +15,24 @@ var app = connect().use(rewrite).use(function(req, res) {
   res.end('test');
 });
 
+console.log('Starting server.');
+
 var port = 9001;
 var server = http.createServer(app).listen(port, function() {
-  console.log('Listening on port ' + port);
+  console.log('Sending request.');
+
+  request('http://127.0.0.1:' + port, function(err, response, body) {
+    console.log('Received response.');
+
+    if (err) {
+      assert.fail('Error in request.');
+    }
+
+    if (!/rewrite/i.test(body)) {
+      assert.fail('"rewrite" string not found in response body.');
+    }
+
+    console.log('Success.');
+    process.exit();
+  });
 });
